dc42a9c6e0748148a468a997a784dcc8e21e5d19
mellamoeterno <richmyself8@gmail.com>
2026-04-23 23:45:37 -0300

    nw

---
+++ firstThings.c
@@ -0,0 +1,11 @@
+#include <stdio.h> //this whole statement is a preprocessor directive
+                        //it tells the compilet to include the standard input output library
+
+    int main(){
+        printf("i like pineapple\n");
+        printf("its really good");
+
+        return 0;
+    }
+
+    //this whole int main(){} is necessary for the code to work, without it wnt.
---
+++ secondThings.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int age = 25;
+    int year= 2025;
+    int quantity = 1;
+
+    printf("you are %d years old\n", age);
+    printf("The year is %d\n", year);
+    printf("you have ordered %d\n", quantity);
+
+    return 0;
+}
